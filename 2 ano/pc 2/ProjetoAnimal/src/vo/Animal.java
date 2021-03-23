e5d944f3e17b84bb6dfeb84c714b4a828388c17c
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-03-23 11:27:59 -0300

    pc2

---
+++ 2 ano/pc 2/ProjetoAnimal/src/vo/Animal.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+/**
+ *
+ * @author JP
+ */
+public class Animal {
+    private int codigo;
+    private String nome;
+    private String proprietario;
+    private int idade;
+    private String especie;
+    private String raca;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the proprietario
+     */
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    /**
+     * @param proprietario the proprietario to set
+     */
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return the raca
+     */
+    public String getRaca() {
+        return raca;
+    }
+
+    /**
+     * @param raca the raca to set
+     */
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+}

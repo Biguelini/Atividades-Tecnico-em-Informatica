b9f7bd9beb7026c8a2aadfe5e306ee714b61c1f6
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-04-27 12:18:35 -0300

    finalizado

--- 2 ano/pc 2/BdJavaList/src/tela/TelaLocaliza.java
+++ 2 ano/pc 2/BdJavaList/src/tela/TelaLocaliza.java
@@ -5,17 +5,42 @@
  */
 package tela;
 
+import bd.BdAluno;
+import java.util.List;
+import java.util.Iterator;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import vo.Aluno;
+
 /**
  *
  * @author JP
  */
 public class TelaLocaliza extends javax.swing.JFrame {
 
+    BdAluno bd;
+
     /**
      * Creates new form TelaLocaliza
      */
     public TelaLocaliza() {
         initComponents();
+        bd = new BdAluno();
+        preencheTabela();
+    }
+
+    private void preencheTabela() {
+        DefaultTableModel modelo = (DefaultTableModel) tLocaliza.getModel();
+        int i = modelo.getRowCount();
+        while (i-- > 0) {
+            modelo.removeRow(i);
+        }
+        List c = bd.pesquisa(tFiltro.getText());
+        for (Iterator it = c.iterator(); it.hasNext();) {
+            Aluno a = (Aluno) it.next();
+            modelo.addRow(new Object[]{a.getCodigo(), a.getNome(), a.getSerie(),
+                a.getTurma()});
+        }
     }
 
     /**
@@ -27,34 +52,86 @@ public class TelaLocaliza extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        tFiltro = new javax.swing.JTextField();
+        bFiltro = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tLocaliza = new javax.swing.JTable();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
+        mArquivo = new javax.swing.JMenu();
+        mNovo = new javax.swing.JMenuItem();
+        mAlterar = new javax.swing.JMenuItem();
+        mExcluir = new javax.swing.JMenuItem();
+        mSair = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                formFocusGained(evt);
+            }
+        });
 
-        jMenu1.setText("Arquivo");
+        bFiltro.setText("Ok");
+        bFiltro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bFiltroActionPerformed(evt);
+            }
+        });
 
-        jMenuItem1.setLabel("Novo");
-        jMenuItem1.setName("mNovo"); // NOI18N
-        jMenu1.add(jMenuItem1);
+        jLabel1.setText("Filtro:");
 
-        jMenuItem2.setLabel("Alterar");
-        jMenuItem2.setName("mAlterar"); // NOI18N
-        jMenu1.add(jMenuItem2);
+        tLocaliza.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Código", "Nome", "Série", "Turma"
+            }
+        ));
+        jScrollPane1.setViewportView(tLocaliza);
 
-        jMenuItem3.setLabel("Excluir");
-        jMenuItem3.setName("mExcluir"); // NOI18N
-        jMenu1.add(jMenuItem3);
+        mArquivo.setText("Arquivo");
 
-        jMenuItem4.setLabel("Sair");
-        jMenuItem4.setName("mSair"); // NOI18N
-        jMenu1.add(jMenuItem4);
+        mNovo.setLabel("Novo");
+        mNovo.setName("mNovo"); // NOI18N
+        mNovo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mNovoActionPerformed(evt);
+            }
+        });
+        mArquivo.add(mNovo);
 
-        jMenuBar1.add(jMenu1);
+        mAlterar.setLabel("Alterar");
+        mAlterar.setName("mAlterar"); // NOI18N
+        mAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mAlterarActionPerformed(evt);
+            }
+        });
+        mArquivo.add(mAlterar);
+
+        mExcluir.setLabel("Excluir");
+        mExcluir.setName("mExcluir"); // NOI18N
+        mExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mExcluirActionPerformed(evt);
+            }
+        });
+        mArquivo.add(mExcluir);
+
+        mSair.setLabel("Sair");
+        mSair.setName("mSair"); // NOI18N
+        mSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mSairActionPerformed(evt);
+            }
+        });
+        mArquivo.add(mSair);
+
+        jMenuBar1.add(mArquivo);
 
         setJMenuBar(jMenuBar1);
 
@@ -62,15 +139,74 @@ public class TelaLocaliza extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 502, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(tFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(bFiltro)))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(tFiltro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(bFiltro)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 376, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bFiltroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bFiltroActionPerformed
+        preencheTabela();
+    }//GEN-LAST:event_bFiltroActionPerformed
+
+    private void mNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mNovoActionPerformed
+        TelaAluno t = new TelaAluno();
+        t.setAluno(new Aluno());
+        t.setVisible(true);
+    }//GEN-LAST:event_mNovoActionPerformed
+
+    private void mAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mAlterarActionPerformed
+        DefaultTableModel modelo = (DefaultTableModel) tLocaliza.getModel();
+        if (tLocaliza.getSelectedRow() != -1) {
+            int codigo = (Integer) modelo.getValueAt(tLocaliza.getSelectedRow(), 0);
+            TelaAluno t = new TelaAluno();
+            t.setAluno(bd.localiza(codigo));
+            t.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(this, "Não há nenhum aluno selecionado");
+        }
+    }//GEN-LAST:event_mAlterarActionPerformed
+
+    private void mExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mExcluirActionPerformed
+        DefaultTableModel modelo = (DefaultTableModel) tLocaliza.getModel();
+        if (tLocaliza.getSelectedRow() != -1) {
+            int codigo = (Integer) modelo.getValueAt(tLocaliza.getSelectedRow(), 0);
+            bd.exclui(codigo);
+            preencheTabela();
+        } else {
+            JOptionPane.showMessageDialog(this, "Não há nenhum aluno selecionado");
+        }
+    }//GEN-LAST:event_mExcluirActionPerformed
+
+    private void formFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_formFocusGained
+        preencheTabela();
+    }//GEN-LAST:event_formFocusGained
+
+    private void mSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mSairActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_mSairActionPerformed
 
     /**
      * @param args the command line arguments
@@ -108,11 +244,16 @@ public class TelaLocaliza extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
+    private javax.swing.JButton bFiltro;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JMenuItem mAlterar;
+    private javax.swing.JMenu mArquivo;
+    private javax.swing.JMenuItem mExcluir;
+    private javax.swing.JMenuItem mNovo;
+    private javax.swing.JMenuItem mSair;
+    private javax.swing.JTextField tFiltro;
+    private javax.swing.JTable tLocaliza;
     // End of variables declaration//GEN-END:variables
 }

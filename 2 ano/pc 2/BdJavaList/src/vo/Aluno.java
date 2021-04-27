7a54344b71e1b1ce5f77dab57afce048ef09f6ab
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-04-27 11:32:10 -0300

    criação do menu

--- 2 ano/pc 2/BdJavaList/src/vo/Aluno.java
+++ 2 ano/pc 2/BdJavaList/src/vo/Aluno.java
@@ -15,4 +15,60 @@ public class Aluno {
     private String nome;
     private int serie;
     private String turma;
+
+    /**
+     * @return the codigo
+     */
+    public int getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the serie
+     */
+    public int getSerie() {
+        return serie;
+    }
+
+    /**
+     * @param serie the serie to set
+     */
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+
+    /**
+     * @return the turma
+     */
+    public String getTurma() {
+        return turma;
+    }
+
+    /**
+     * @param turma the turma to set
+     */
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
 }

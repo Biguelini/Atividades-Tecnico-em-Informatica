d1c74a4bc68936e7a63047f06a836318b883688a
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-05-10 17:16:56 -0300

    update

---
+++ 2 ano/pc 2/CadastroEquipamentos/src/bd/Bd.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+/**
+ *
+ * @author 2info2021
+ */
+import java.sql.*;
+
+public class Bd {
+
+    private static Connection con = null;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String url = "jdbc:mysql://localhost/produtos";
+    private static final String usuario = "root";
+    private static final String senha = "vertrigo";
+
+    public static Connection getCon() {
+        try {
+            if (con == null || con.isClosed()) {
+                Class.forName(driver);
+                con = DriverManager.getConnection(url, usuario, senha);
+            }
+        } catch (Exception e) {
+            System.out.println(" erro na conexao\n" + e.getMessage());
+        }
+        return con;
+    }
+
+    public static void setCon(Connection cone) {
+        con = cone;
+    }
+}

a6ae265a0a8062c3ca87afe017b3553fac043401
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-05-10 13:59:58 -0300

    rodou

---
+++ 2 ano/pc 1/CadastroProdutos/CadastroProdutos/Form1.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArquivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AlterarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExcluirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.dgfiltro = New System.Windows.Forms.DataGridView()
+        Me.txtfiltro = New System.Windows.Forms.TextBox()
+        Me.btnfiltro = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.dgfiltro, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArquivoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(765, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArquivoToolStripMenuItem
+        '
+        Me.ArquivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoToolStripMenuItem, Me.AlterarToolStripMenuItem, Me.ExcluirToolStripMenuItem})
+        Me.ArquivoToolStripMenuItem.Name = "ArquivoToolStripMenuItem"
+        Me.ArquivoToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ArquivoToolStripMenuItem.Text = "Arquivo"
+        '
+        'NovoToolStripMenuItem
+        '
+        Me.NovoToolStripMenuItem.Name = "NovoToolStripMenuItem"
+        Me.NovoToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.NovoToolStripMenuItem.Text = "Novo"
+        '
+        'AlterarToolStripMenuItem
+        '
+        Me.AlterarToolStripMenuItem.Name = "AlterarToolStripMenuItem"
+        Me.AlterarToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.AlterarToolStripMenuItem.Text = "Alterar"
+        '
+        'ExcluirToolStripMenuItem
+        '
+        Me.ExcluirToolStripMenuItem.Name = "ExcluirToolStripMenuItem"
+        Me.ExcluirToolStripMenuItem.Size = New System.Drawing.Size(109, 22)
+        Me.ExcluirToolStripMenuItem.Text = "Excluir"
+        '
+        'dgfiltro
+        '
+        Me.dgfiltro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgfiltro.Location = New System.Drawing.Point(13, 59)
+        Me.dgfiltro.Name = "dgfiltro"
+        Me.dgfiltro.Size = New System.Drawing.Size(740, 273)
+        Me.dgfiltro.TabIndex = 1
+        '
+        'txtfiltro
+        '
+        Me.txtfiltro.Location = New System.Drawing.Point(13, 28)
+        Me.txtfiltro.Name = "txtfiltro"
+        Me.txtfiltro.Size = New System.Drawing.Size(136, 20)
+        Me.txtfiltro.TabIndex = 2
+        '
+        'btnfiltro
+        '
+        Me.btnfiltro.Location = New System.Drawing.Point(155, 27)
+        Me.btnfiltro.Name = "btnfiltro"
+        Me.btnfiltro.Size = New System.Drawing.Size(75, 23)
+        Me.btnfiltro.TabIndex = 3
+        Me.btnfiltro.Text = "Filtro"
+        Me.btnfiltro.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(765, 344)
+        Me.Controls.Add(Me.btnfiltro)
+        Me.Controls.Add(Me.txtfiltro)
+        Me.Controls.Add(Me.dgfiltro)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.dgfiltro, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ArquivoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NovoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AlterarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExcluirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents dgfiltro As System.Windows.Forms.DataGridView
+    Friend WithEvents txtfiltro As System.Windows.Forms.TextBox
+    Friend WithEvents btnfiltro As System.Windows.Forms.Button
+
+End Class
---
+++ 2 ano/pc 1/CadastroProdutos/CadastroProdutos/Produto.vb
@@ -0,0 +1,8 @@
+﻿Public Class Produto
+    Public id As Integer
+    Public descricao As String
+    Public fabricante As String
+    Public numserie As String
+    Public numpatrimonio As Integer
+    Public localizacao As String
+End Class

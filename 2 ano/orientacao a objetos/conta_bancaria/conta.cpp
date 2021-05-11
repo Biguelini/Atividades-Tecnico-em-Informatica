5d0e3d01d30d4d64edb79eb8fa50c2ef91e4e8e1
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-05-11 11:13:51 -0300

    poo

---
+++ 2 ano/orientacao a objetos/conta_bancaria/conta.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Conta {
+private:
+    float saldo = 0;
+
+public:
+    void creditar(float x)
+    {
+        saldo += x;
+    };
+    float getSaldo()
+    {
+        return saldo;
+    };
+};
+int main() {
+    Conta c1, c2;
+    c1.creditar(500);
+    c2.creditar(750);
+    cout << c1.getSaldo() << endl;
+    cout << c2.getSaldo() << endl;
+    return 0;
+}

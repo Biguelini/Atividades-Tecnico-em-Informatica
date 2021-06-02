04ac10b5199225f147c538cbcdacacfd4881e513
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-06-02 16:37:03 -0300

    update

---
+++ 2 ano/sistemas web/CadastroFilmes/src/java/vo/Filme.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 2info2021
+ */
+@Entity
+public class Filme implements Serializable {
+
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titulo;
+    private String diretor;
+    private String genero;
+    private int duracao;
+
+    public Filme(Integer id) {
+        this.id = id;
+    }
+
+    public Filme() {
+    }
+
+    /**
+     * @return the serialVersionUID
+     */
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @param aSerialVersionUID the serialVersionUID to set
+     */
+    public static void setSerialVersionUID(long aSerialVersionUID) {
+        serialVersionUID = aSerialVersionUID;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the diretor
+     */
+    public String getDiretor() {
+        return diretor;
+    }
+
+    /**
+     * @param diretor the diretor to set
+     */
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    /**
+     * @return the genero
+     */
+    public String getGenero() {
+        return genero;
+    }
+
+    /**
+     * @param genero the genero to set
+     */
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    /**
+     * @return the duracao
+     */
+    public int getDuracao() {
+        return duracao;
+    }
+
+    /**
+     * @param duracao the duracao to set
+     */
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+}

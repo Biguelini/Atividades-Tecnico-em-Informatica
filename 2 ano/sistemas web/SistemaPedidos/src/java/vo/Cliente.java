b15f6f5d5ca128a33a0712ae1a8a99536adc4d17
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-08-27 15:17:48 -0300

    inicio vo ok

---
+++ 2 ano/sistemas web/SistemaPedidos/src/java/vo/Cliente.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author 2info2021
+ */
+@Entity
+public class Cliente implements Serializable {
+    private static long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String endereço;
+    private int telefone;
+    private int cpf;
+    
+    public Cliente(Integer id) {
+        this.id = id;
+    }
+    
+    public Cliente() {
+    }
+
+    /**
+     * @return the serialVersionUID
+     */
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    /**
+     * @param aSerialVersionUID the serialVersionUID to set
+     */
+    public static void setSerialVersionUID(long aSerialVersionUID) {
+        serialVersionUID = aSerialVersionUID;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the endereço
+     */
+    public String getEndereço() {
+        return endereço;
+    }
+
+    /**
+     * @param endereço the endereço to set
+     */
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public int getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public int getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+}

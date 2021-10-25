28a14429b8a1dd4b6d2d87674eca8d1e4d2f35ba
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-10-25 17:16:38 -0300

    update

--- 2 ano/pc 2/snake/src/jogo/Snake.java
+++ 2 ano/pc 2/snake/src/jogo/Snake.java
@@ -10,10 +10,19 @@ package jogo;
  * @author JP
  */
 import javax.swing.JFrame;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
 
 public class Snake extends JFrame {
 
-    public Snake() {
+    public Snake() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        AudioInputStream audioInputStream;
+        audioInputStream = AudioSystem.getAudioInputStream(new File("C:/Users/2info2021/Desktop/musica.mp3").getAbsoluteFile());
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
+        clip.loop(Clip.LOOP_CONTINUOUSLY); //Para repetir o som.
         setTitle("Snake v1.0 - By Biguelini");
         setSize(1000, 733);
         add(new Campo(1000, 733));
@@ -22,7 +31,7 @@ public class Snake extends JFrame {
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Snake();
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Snake snake = new Snake();
     }
 }

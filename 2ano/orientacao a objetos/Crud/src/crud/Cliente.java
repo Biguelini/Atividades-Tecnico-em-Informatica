0a81f88f33f1776b509f08d07010ffddf7fe7a91
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-11-23 20:35:43 -0300

    update

---
+++ 2ano/orientacao a objetos/Crud/src/crud/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crud;
+
+/**
+ *
+ * @author JP
+ */
+public class Cliente {
+    private String nome;
+    private String fone;
+    public Cliente(){
+        
+    }
+}

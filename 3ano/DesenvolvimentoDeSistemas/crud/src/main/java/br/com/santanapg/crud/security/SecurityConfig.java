1736f3c33d5681246dfa23a954c60d3d9c1ef9c8
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-02-10 16:07:10 -0300

    implementando segurança

---
+++ 3ano/DesenvolvimentoDeSistemas/crud/src/main/java/br/com/santanapg/crud/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.santanapg.crud.security;
+
+/**
+ *
+ * @author 2info2021
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}pindamonhangaba")
+                .roles("USER");
+    }
+}

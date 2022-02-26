64b696c2651d6b019773b9d3e759a8ccd3f8be27
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-02-26 17:47:31 -0300

    update

--- 3ano/DesenvolvimentoDeSistemas/financeiro/movimentacao/views.py
+++ 3ano/DesenvolvimentoDeSistemas/financeiro/movimentacao/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import FormRegistrar, Transacao, Saldo
@@ -10,19 +11,32 @@ from django.contrib.auth.decorators import login_required
 
 
 def index(request):
-    if not Saldo.objects.filter(id=1):
-        saldo = Saldo(id=1, valor=0)
+    existe = True
+    data_atual = datetime.today().strftime("%Y-%m-%d")
+    data_formatada = datetime.today().strftime("%d/%m/%Y")
+    saldos = Saldo.objects.order_by('-data')
+    meu_saldo = 0
+    for saldo in saldos:
+        if data_atual == saldo.data.strftime("%Y-%m-%d"):
+            existe = True
+            print(data_atual, saldo.data.strftime("%Y-%m-%d"))
+        else:
+            existe = False
+        meu_saldo += saldo.valor
+    if not existe:
+        saldo = Saldo(valor=0)
         saldo.save()
-    saldo = get_object_or_404(Saldo, pk=1)
+    print(meu_saldo)
     transacoes = Transacao.objects.order_by('-data')
     paginator = Paginator(transacoes, 5)
     page = request.GET.get('p')
     transacoes = paginator.get_page(page)
     num_transacoes = len(transacoes)
-    saldo = round(saldo.valor, 2)
+    meu_saldo = round(meu_saldo, 2)
     return render(request, 'movimentacao/index.html', {
         'transacoes': transacoes,
-        'saldo': saldo,
+        'saldo': meu_saldo,
+        'data': data_formatada,
         'num_transacoes': num_transacoes,
     })
 
@@ -50,6 +64,7 @@ def logout(request):
 
 
 def registrar(request):
+    existe = True
     if request.method != 'POST':
         form = FormRegistrar()
         return render(request, 'movimentacao/registrar.html', {'form': form})
@@ -59,18 +74,28 @@ def registrar(request):
         messages.error(request, 'Erro ao enviar o formulário')
         form = FormRegistrar(request.POST)
         return render(request, 'movimentacao/registrar.html', {'form': form})
-    if not Saldo.objects.filter(id=1):
-        saldo = Saldo(id=1, valor=0)
+    
+    data_atual = datetime.today().strftime("%Y-%m-%d")
+    saldos = Saldo.objects.order_by('-data')
+    meu_saldo = 0
+    for saldo in saldos:
+        if data_atual == saldo.data.strftime("%Y-%m-%d"):
+            existe = True
+        else:
+            existe = False
+        meu_saldo += saldo.valor
+    if not existe:
+        saldo = Saldo(valor=0)
         saldo.save()
-
     tipo = request.POST.get('tipo')
-    saldo = get_object_or_404(Saldo, pk=1)
-    if(tipo == '1'):
-        saldo.valor -= int(request.POST.get('valor'))
-    else:
-        saldo.valor += int(request.POST.get('valor'))
+    for saldo in saldos:
+        if data_atual == saldo.data.strftime("%Y-%m-%d"):
+            if(tipo == '1'):
+                saldo.valor -= int(request.POST.get('valor'))
+            else:
+                saldo.valor += int(request.POST.get('valor'))
+        saldo.save()
     form.save()
-    saldo.save()
     messages.success(
         request, f"{request.POST.get('descricao')} registrado com sucesso")
     return redirect('registrar')

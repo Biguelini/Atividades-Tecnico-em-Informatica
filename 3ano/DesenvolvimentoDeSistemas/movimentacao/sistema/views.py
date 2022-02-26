90ff0f8b8a1562fd632e42beffaa04184e4a5170
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-02-25 21:17:58 -0300

    dando certo

--- 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
+++ 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
@@ -70,4 +70,41 @@ def registrar(request):
 
 
 def busca(request):
-    pass
+
+    datamin = request.GET.get('datamin')
+    datamax = request.GET.get('datamax')
+    if datamin != "" and datamax != "":
+        if(datamin < datamax):
+            existe = False
+            # pegando a data atual
+            data_formatada = datetime.today().strftime("%d/%m/%Y")
+            data_atual = datetime.today().strftime("%Y-%m-%d")
+            # saldo
+            meu_saldo_total = 0
+            meu_saldo_hoje = 0
+            saldos = Saldo.objects.order_by('data')
+            for saldo in saldos:
+                if not existe:
+                    if data_atual == saldo.data.strftime("%Y-%m-%d"):
+                        existe = True
+                        meu_saldo_hoje += saldo.valor
+                    else:
+                        existe = False
+                meu_saldo_total+=saldo.valor
+            if not existe:
+                saldo = Saldo(valor=0)
+                saldo.save()
+                existe = True
+                
+            # transacoes e paginacao
+            transacoes = Transacao.objects.order_by('-data').filter(data__gt=datamin, data__lt=datamax)
+            num_transacoes = len(transacoes)
+            return render(request, 'sistema/busca.html', {
+                'transacoes': transacoes,
+                'saldo_total': meu_saldo_total,
+                'saldo_hoje': meu_saldo_hoje,
+                'data': data_formatada,
+                'num_transacoes': num_transacoes,
+            })
+    messages.error(request, 'Preencha as datas')
+    return redirect('index')

fba649aa3cb0f2d4797b1f5e9bfe81189746a940
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-03-21 16:16:22 -0300

    update

--- 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
+++ 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
@@ -18,7 +18,11 @@ def index(request):
             current_balance_exists = True
         else:
 <<<<<<< HEAD
+<<<<<<< HEAD
             if(int(datetime.today().strftime('%H'))>=21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
+=======
+            if(int(datetime.today().strftime('%H')) >= 21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
+>>>>>>> parent of 2e0ce5d (Update views.py)
 =======
             if(int(datetime.today().strftime('%H')) >= 21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
 >>>>>>> parent of 2e0ce5d (Update views.py)

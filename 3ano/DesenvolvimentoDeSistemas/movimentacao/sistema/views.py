ae450f5056e8fa46e4f7a948ef88805a3ca47ff6
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-03-21 16:37:48 -0300

    update

--- 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
+++ 3ano/DesenvolvimentoDeSistemas/movimentacao/sistema/views.py
@@ -10,26 +10,14 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     balance_yesterday = 0
     my_balance = 0
-    
+
     current_balance_exists = False
     for balance in Balances.objects.order_by('date'):
         balance_date = balance.date.strftime("%Y-%m-%d")
         if datetime.today().strftime("%Y-%m-%d") == balance_date:
             current_balance_exists = True
         else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            if(int(datetime.today().strftime('%H'))>=21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
-=======
             if(int(datetime.today().strftime('%H')) >= 21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
->>>>>>> parent of 2e0ce5d (Update views.py)
-=======
-            if(int(datetime.today().strftime('%H')) >= 21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
->>>>>>> parent of 2e0ce5d (Update views.py)
-=======
-            if(int(datetime.today().strftime('%H')) >= 21 and balance_date == (datetime.today()+timedelta(days=1)).strftime("%Y-%m-%d")):
->>>>>>> parent of 2e0ce5d (Update views.py)
                 current_balance_exists = True
                 my_balance = balance.value
             else:
@@ -37,10 +25,10 @@ def index(request):
                     current_balance_exists = False
         if (datetime.today()-timedelta(days=1)).strftime("%Y-%m-%d") == balance_date:
             balance_yesterday = balance.value
-        
+
         my_balance = balance.value
     if not current_balance_exists:
-        balance = Balances(date=datetime.today(),value=balance_yesterday)
+        balance = Balances(date=datetime.today(), value=balance_yesterday)
         balance.save()
         current_balance_exists = True
 
@@ -61,8 +49,8 @@ def login(request):
     if request.method != 'POST':
         return render(request, 'sistema/login.html')
 
-    user_request = auth.authenticate(request, useername=request.POST.get('usuario'), password=request.POST.get('senha'))
-
+    user_request = auth.authenticate(request, username=request.POST.get(
+        'username'), password=request.POST.get('password'))
     if not user_request:
         messages.error(request, 'Usuário ou senha inválidos')
         return render(request, 'sistema/login.html')
@@ -74,8 +62,13 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('index')
+
+
 @login_required(login_url='/', redirect_field_name='')
 def register_movement(request):
+    balance_yesterday = 0
+    is_the_last = False
+    current_balance_exists = False
     if request.method != 'POST':
         form = RegisterForm()
         return render(request, 'sistema/register_movement.html', {'form': form})
@@ -84,34 +77,77 @@ def register_movement(request):
         messages.error(request, 'Erro ao enviar o formulário')
         form = RegisterForm(request.POST)
         return render(request, 'sistema/register_movement.html', {'form': form})
-
+    date = datetime.strptime(request.POST.get('date'), '%Y-%m-%d')
+    type = request.POST.get('type')
+    value = request.POST.get('value')
+    for balance in Balances.objects.order_by('date'):
+        balance_date = datetime.strptime(balance.date.strftime("%Y-%m-%d"), '%Y-%m-%d')
+        if date == balance_date or balance_date > date:
+            if type == '1':
+                modified_balance = float(balance.value) - float(value)
+            else:
+                modified_balance = float(balance.value) + float(value)
+            balance.value = modified_balance
+            balance.save()
+            
+            
+        if date == balance_date:
+            current_balance_exists = True
+        else:
+            if not current_balance_exists:
+                current_balance_exists = False
+        if (date > balance_date):
+            if type == '1':
+                balance_yesterday = float(balance.value) - float(request.POST.get('value'))
+            else:
+                balance_yesterday = float(balance.value) + float(request.POST.get('value'))
+    
+    for balance in Balances.objects.order_by('-date'):  
+        if (date<balance_date):
+            if type == '1':
+                balance_yesterday = 0 - float(request.POST.get('value'))
+            else:
+                balance_yesterday = float(request.POST.get('value'))
+            
+    if not current_balance_exists:
+        balance = Balances(date=date, value=balance_yesterday)
+        balance.save()
+        current_balance_exists = True
+    form.save()
+            
 
     messages.success(
         request, f"{request.POST.get('description')} registrado com sucesso")
     return redirect('register_movement')
 
+
 def receipt(request):
-    
+
     mindate = request.GET.get('mindate')
     maxdate = request.GET.get('maxdate')
-    balance_of_date = 0
-    
+    inicial_balance = 0
+    final_balance=0
+
     if mindate != "" and maxdate != "":
         maxdate = datetime.strptime(maxdate, '%Y-%m-%d')
-        maxdate= (maxdate+timedelta(days=1)).strftime("%Y-%m-%d")
+        maxdate = (maxdate+timedelta(days=1)).strftime("%Y-%m-%d")
+        mindate = datetime.strptime(mindate, '%Y-%m-%d')
+        mindate = (mindate-timedelta(days=1)).strftime("%Y-%m-%d")
         if(mindate < maxdate or mindate == maxdate):
+            for balance in Balances.objects.order_by('-date'):
+                if mindate <= balance.date.strftime("%Y-%m-%d") and maxdate > balance.date.strftime("%Y-%m-%d"):
+                    inicial_balance = balance.value
             for balance in Balances.objects.order_by('date'):
                 if mindate <= balance.date.strftime("%Y-%m-%d") and maxdate > balance.date.strftime("%Y-%m-%d"):
-                    balance_of_date = balance.value
-                my_total_balance = balance.value
+                    final_balance = balance.value
 
             # transacoes e paginacao
             transactions = Transactions.objects.order_by(
                 '-date').filter(date__gt=mindate, date__lt=maxdate)
             return render(request, 'sistema/receipt.html', {
                 'transactions': transactions,
-                'my_total_balance': my_total_balance,
-                'balance_of_date': balance_of_date,
+                'final_balance': final_balance,
+                'inicial_balance': inicial_balance,
                 'num_transactions': len(transactions),
             })
     messages.error(request, 'Preencha as datas corretamente')

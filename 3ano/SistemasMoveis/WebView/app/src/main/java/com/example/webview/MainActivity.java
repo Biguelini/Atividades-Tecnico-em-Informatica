6439617d7d916de3da78649d2438be70e0363b71
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-04-05 16:50:44 -0300

    update

---
+++ 3ano/SistemasMoveis/WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setSupportZoom(true);
+        webview.getSettings().setAppCacheEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://sites.google.com/view/biguelini");
+    }
+}

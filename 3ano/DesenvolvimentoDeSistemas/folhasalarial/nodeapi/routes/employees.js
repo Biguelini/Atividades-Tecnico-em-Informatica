daf7f4ac7ad7c511fe5efca1b92e990bfe1c6730
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-04-09 16:54:40 -0300

    Update employees.js

--- 3ano/DesenvolvimentoDeSistemas/folhasalarial/nodeapi/routes/employees.js
+++ 3ano/DesenvolvimentoDeSistemas/folhasalarial/nodeapi/routes/employees.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const mysql = require('../mysql').pool
 
+function isFloat(x) {
+    return !!(x % 1)
+}
+
 router.get('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) {
@@ -25,8 +29,9 @@ router.get('/', (req, res, next) => {
                         wantMealVouchers: employee.wantMealVouchers,
                         request: {
                             type: 'GET',
-                            description: 'Returns all registered employees',
-                            url: 'localhost:3000/employees/' + employee.id,
+                            description:
+                                'Returns the details of a specific employee',
+                            url: 'localhost:3000/employee/' + employee.id,
                         },
                     }
                 }),
@@ -41,27 +46,42 @@ router.post('/', (req, res, next) => {
         if (error) {
             return res.status(500).send({ error: error })
         }
-        conn.query(
-            'INSERT INTO employees (name, baseSalary, numberOfChildrenUnderFourteen, numberOfIncomeTaxDependents, wantMealVouchers) VALUES (?,?,?,?,?)',
-            [
-                req.body.name,
-                req.body.baseSalary,
-                req.body.numberOfChildrenUnderFourteen,
-                req.body.numberOfIncomeTaxDependents,
-                req.body.wantMealVouchers,
-            ],
-            (error, result, fields) => {
-                conn.release()
-                if (error) {
-                    return res.status(500).send({ error: error })
+            conn.query(
+                'INSERT INTO employees (name, baseSalary, numberOfChildrenUnderFourteen, numberOfIncomeTaxDependents, wantMealVouchers) VALUES (?,?,?,?,?)',
+                [
+                    req.body.name,
+                    req.body.baseSalary,
+                    req.body.numberOfChildrenUnderFourteen,
+                    req.body.numberOfIncomeTaxDependents,
+                    req.body.wantMealVouchers,
+                ],
+                (error, result, fields) => {
+                    conn.release()
+                    if (error) {
+                        return res.status(500).send({ error: error })
+                    }
+                    const response = {
+                        message: 'Employee registered successfully',
+                        registeredEmployee: {
+                            name: req.body.name,
+                            baseSalary: req.body.baseSalary,
+                            numberOfChildrenUnderFourteen:
+                                req.body.numberOfChildrenUnderFourteen,
+                            numberOfIncomeTaxDependents:
+                                req.body.numberOfIncomeTaxDependents,
+                            wantMealVouchers: req.body.wantMealVouchers,
+                            request: {
+                                type: 'GET',
+                                description: 'Returns all employees',
+                                url: 'localhost:3000/employees',
+                            },
+                        },
+                    }
+                    return res.status(201).send(response)
                 }
-                res.status(201).send({
-                    message: 'Funcionário inserido com sucesso',
-                    idEmployee: result.insertId,
-                })
-            }
-        )
-    })
+            )
+        }
+)
 })
 
 router.get('/:id_employee', (req, res, next) => {
@@ -76,7 +96,29 @@ router.get('/:id_employee', (req, res, next) => {
                 if (error) {
                     return res.status(500).send({ error: error })
                 }
-                return res.status(200).send({ employee: result })
+                if (result.length == 0) {
+                    return res
+                        .status(404)
+                        .send({ message: 'Employee not found' })
+                }
+                const response = {
+                    Employee: {
+                        id: result[0].id,
+                        name: result[0].name,
+                        baseSalary: result[0].baseSalary,
+                        numberOfChildrenUnderFourteen:
+                            result[0].numberOfChildrenUnderFourteen,
+                        numberOfIncomeTaxDependents:
+                            result[0].numberOfIncomeTaxDependents,
+                        wantMealVouchers: result[0].wantMealVouchers,
+                        request: {
+                            type: 'GET',
+                            description: 'Returns all employees',
+                            url: 'localhost:3000/employees',
+                        },
+                    },
+                }
+                return res.status(200).send(response)
             }
         )
     })
@@ -101,9 +143,26 @@ router.patch('/', (req, res, next) => {
                 if (error) {
                     return res.status(500).send({ error: error })
                 }
-                res.status(202).send({
-                    message: 'Funcionário alterado com sucesso',
-                })
+                const response = {
+                    message: 'Employee successfully modified',
+                    modifiedEmployee: {
+                        id: req.body.id,
+                        name: req.body.name,
+                        baseSalary: req.body.baseSalary,
+                        numberOfChildrenUnderFourteen:
+                            req.body.numberOfChildrenUnderFourteen,
+                        numberOfIncomeTaxDependents:
+                            req.body.numberOfIncomeTaxDependents,
+                        wantMealVouchers: req.body.wantMealVouchers,
+                        request: {
+                            type: 'GET',
+                            description:
+                                'Returns the details of a specific employee',
+                            url: 'localhost:3000/employee/' + req.body.id,
+                        },
+                    },
+                }
+                return res.status(202).send(response)
             }
         )
     })
@@ -121,8 +180,23 @@ router.delete('/', (req, res, next) => {
                 if (error) {
                     return res.status(500).send({ error: error })
                 }
+                const responde = {
+                    message: 'Employee successfully removed',
+                    request: {
+                        type: 'POST',
+                        description: 'Register an employee',
+                        url: 'localhost:3000/employees',
+                        body: {
+                            name: 'String',
+                            baseSalary: 'Float',
+                            numberOfChildrenUnderFourteen: 'Integer',
+                            numberOfIncomeTaxDependents: 'Integer',
+                            wantMealVouchers: 'Bool(1 or 0)',
+                        },
+                    },
+                }
                 res.status(202).send({
-                    message: 'Funcionário removido com sucesso',
+                    message: 'Employee successfully removed',
                 })
             }
         )

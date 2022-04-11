99e183e2aa0f80929fcf1c7684bf54117cb03dd6
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-04-11 16:10:04 -0300

    não deu certo

--- 3ano/DesenvolvimentoDeSistemas/folhasalarial/nodeapi/mysql.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql')
-var pool = mysql.createPool({
-    "user": "root",
-    "password": "vertrigo",
-    "database": "folhasalarial",
-    "host": "localhost",
-    "port": 3306,
-})
-exports.pool = pool

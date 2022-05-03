1d8d658855563cb283172f02235f84ab8ea26a19
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-05-03 15:24:29 -0300

    Update main.js

--- 3ano/DesenvolvimentoDeSistemas/folhasalarial/website/main.js
+++ 3ano/DesenvolvimentoDeSistemas/folhasalarial/website/main.js
@@ -144,6 +144,7 @@ function deleteEmployee(id) {
         text = 'Operação cancelada!'
     }
 }
+function getTables() {}
 function calcSalary() {
     const url = 'http://localhost:3000/employee'
     fetch(url)
@@ -152,84 +153,162 @@ function calcSalary() {
         })
         .then(function (data) {
             const employees = data
-            for (var line in employees) {
-                var name = employees[line].name
-                var baseSalary = employees[line].baseSalary
-                var inss = calcINSS(baseSalary)
-                var familySalary =
-                    baseSalary <= 1655.98
-                        ? employees[line].childrenUnder14YearsOld * 56.47
-                        : 0
-                var vt = employees[line].wantTransportationVouchers
-                    ? calcPercent(baseSalary, 6)
-                    : 0
-                var baseCalcSalary =
-                    baseSalary -
-                    inss -
-                    employees[line].incomeTaxDependent * 189.59
-                var irrf = calcIRFF(baseCalcSalary)
-                const finalSalary = baseSalary - inss + familySalary - vt - irrf
-                var newTableLine = `
-                    <tr>
-                        <td> ${name}</td>
-                        <td>R$ ${baseSalary.toFixed(2)}</td>
-                        <td>R$ ${inss.toFixed(2)}</td>
-                        <td>R$ ${familySalary.toFixed(2)}</td>
-                        <td>R$ ${vt.toFixed(2)}</td>
-                        <td>R$ ${irrf.toFixed(2)}</td>
-                        <td>R$ ${finalSalary.toFixed(2)}</td>
-                        <td class="d-flex justify-content-center">
-                            <a href="./holerite.html?idEmployee=${
-                                employees[line]._id
-                            }"><button
-                        class="btn btn-primary" type="button" >Ver
-                        Holerite</button><a/></td>
-                    </tr>`
-                document.getElementById('tableEmployeesBody').innerHTML +=
-                    newTableLine
-            }
+
+            const urlTables = 'http://localhost:3000/tables'
+            fetch(urlTables)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    const tables = data
+                    const table = tables[0]
+                    const {
+                        firstTierINSS,
+                        secondTierINSS,
+                        thirdTierINSS,
+                        fourthTierINSS,
+                        firstAliquotINSS,
+                        secondAliquotINSS,
+                        thirdAliquotINSS,
+                        fourthAliquotINSS,
+                        maximumEligibleSalaryForFamilySalary,
+                        valueFamilySalary,
+                        firstTierIRRF,
+                        secondTierIRRF,
+                        thirdTierIRRF,
+                        fourthTierIRRF,
+                        secondAliquotIRRF,
+                        thirdAliquotIRRF,
+                        fourthAliquotIRRF,
+                        fifthAliquotIRRF,
+                        secondDeductionIRRF,
+                        thirdDeductionIRRF,
+                        fourthDeductionIRRF,
+                        fifthDeductionIRRF,
+                        dependentDeduction,
+                    } = table
+                    for (var line in employees) {
+                        var name = employees[line].name
+                        var baseSalary = employees[line].baseSalary
+                        var inss = 0
+                        if (baseSalary <= firstTierINSS) {
+                            inss = calcPercent(baseSalary, firstAliquotINSS)
+                        } else if (
+                            baseSalary > firstTierINSS &&
+                            baseSalary <= secondTierINSS
+                        ) {
+                            inss = calcPercent(firstTierINSS, firstAliquotINSS)
+                            inss += calcPercent(
+                                baseSalary - (firstTierINSS + 0.01),
+                                secondAliquotINSS
+                            )
+                        } else if (
+                            baseSalary > secondTierINSS &&
+                            baseSalary <= thirdTierINSS
+                        ) {
+                            inss = calcPercent(firstTierINSS, firstAliquotINSS)
+                            inss += calcPercent(
+                                secondTierINSS - (firstTierINSS + 0.01),
+                                secondAliquotINSS
+                            )
+                            inss += calcPercent(
+                                baseSalary - (secondTierINSS + 0.01),
+                                thirdAliquotINSS
+                            )
+                        } else if (
+                            baseSalary > thirdTierINSS &&
+                            baseSalary <= fourthTierINSS
+                        ) {
+                            inss = calcPercent(firstTierINSS, firstAliquotINSS)
+                            inss += calcPercent(
+                                secondTierINSS - (firstTierINSS + 0.01),
+                                secondAliquotINSS
+                            )
+                            inss += calcPercent(
+                                thirdTierINSS - (secondTierINSS + 0.01),
+                                thirdAliquotINSS
+                            )
+                            inss += calcPercent(
+                                baseSalary - (thirdTierINSS + 0.01),
+                                fourthAliquotINSS
+                            )
+                        } else {
+                            inss = calcPercent(firstTierINSS, firstAliquotINSS)
+                            inss += calcPercent(
+                                secondTierINSS - (firstTierINSS + 0.01),
+                                secondAliquotINSS
+                            )
+                            inss += calcPercent(
+                                thirdTierINSS - (secondTierINSS + 0.01),
+                                thirdAliquotINSS
+                            )
+                            inss += calcPercent(
+                                fourthTierINSS - (thirdTierINSS + 0.01),
+                                fourthAliquotINSS
+                            )
+                        }
+                        var familySalary =
+                            baseSalary <= maximumEligibleSalaryForFamilySalary
+                                ? employees[line].childrenUnder14YearsOld *
+                                  valueFamilySalary
+                                : 0
+                        var vt = employees[line].wantTransportationVouchers
+                            ? calcPercent(baseSalary, 6)
+                            : 0
+                        var baseCalcSalary =
+                            baseSalary -
+                            inss -
+                            employees[line].incomeTaxDependent * dependentDeduction
+                        var irrf = 0
+                        if (baseCalcSalary <= firstTierIRRF) {
+                            irrf = 0
+                        } else if (
+                            baseCalcSalary > firstTierIRRF &&
+                            baseCalcSalary <= secondTierIRRF
+                        ) {
+                            irrf = calcPercent(baseCalcSalary, secondAliquotIRRF) - secondDeductionIRRF
+                        } else if (
+                            baseCalcSalary > secondTierIRRF &&
+                            baseCalcSalary <= thirdTierIRRF
+                        ) {
+                            irrf = calcPercent(baseCalcSalary, thirdAliquotIRRF) - thirdDeductionIRRF
+                        } else if (
+                            baseCalcSalary > thirdTierIRRF &&
+                            baseCalcSalary <= fourthTierIRRF
+                        ) {
+                            irrf = calcPercent(baseCalcSalary, fourthAliquotIRRF) - fourthDeductionIRRF
+                        } else {
+                            irrf = calcPercent(baseCalcSalary, fifthAliquotIRRF) - fifthDeductionIRRF
+                        }
+                        const finalSalary =
+                            baseSalary - inss + familySalary - vt - irrf
+                        var newTableLine = `
+                            <tr>
+                                <td> ${name}</td>
+                                <td>R$ ${baseSalary.toFixed(2)}</td>
+                                <td>R$ ${inss.toFixed(2)}</td>
+                                <td>R$ ${familySalary.toFixed(2)}</td>
+                                <td>R$ ${vt.toFixed(2)}</td>
+                                <td>R$ ${irrf.toFixed(2)}</td>
+                                <td>R$ ${finalSalary.toFixed(2)}</td>
+                                <td class="d-flex justify-content-center">
+                                    <a href="./holerite.html?idEmployee=${
+                                        employees[line]._id
+                                    }"><button
+                                class="btn btn-primary" type="button" >Ver
+                                Holerite</button><a/></td>
+                            </tr>`
+                        document.getElementById(
+                            'tableEmployeesBody'
+                        ).innerHTML += newTableLine
+                    }
+                })
+                .catch(function () {
+                    console.log('Houve algum problema!')
+                })
         })
         .catch(function () {
             console.log('Houve algum problema!')
         })
 }
 const calcPercent = (value, percent) => value * (percent / 100)
-function calcINSS(baseSalary) {
-    var inss = 0
-    if (baseSalary <= 1212) {
-        inss = calcPercent(baseSalary, 7.5)
-    } else if (baseSalary > 1212 && baseSalary <= 2427.36) {
-        inss = calcPercent(1212, 7.5)
-        inss += calcPercent(baseSalary - 1212.01, 9)
-    } else if (baseSalary > 2427.36 && baseSalary <= 3641.03) {
-        inss = calcPercent(1212, 7.5)
-        inss += calcPercent(2427.36 - 1212.01, 9)
-        inss += calcPercent(baseSalary - 2427.37, 12)
-    } else if (baseSalary > 3641.03 && baseSalary <= 7087.22) {
-        inss = calcPercent(1212, 7.5)
-        inss += calcPercent(2427.36 - 1212.01, 9)
-        inss += calcPercent(3641.03 - 2427.37, 12)
-        inss += calcPercent(baseSalary - 3641.04, 14)
-    } else {
-        inss = calcPercent(1212, 7.5)
-        inss += calcPercent(2427.36 - 1212.01, 9)
-        inss += calcPercent(3641.03 - 2427.37, 12)
-        inss += calcPercent(7087.22 - 3641.04, 14)
-    }
-    return inss
-}
-function calcIRFF(baseCalcSalary) {
-    irrf = 0
-    if (baseCalcSalary <= 1903.98) {
-        irrf = 0
-    } else if (baseCalcSalary > 1903.98 && baseCalcSalary <= 2826.65) {
-        irrf = calcPercent(baseCalcSalary, 7.5) - 142.8
-    } else if (baseCalcSalary > 2826.65 && baseCalcSalary <= 3751.05) {
-        irrf = calcPercent(baseCalcSalary, 15) - 354.8
-    } else if (baseCalcSalary > 3751.05 && baseCalcSalary <= 4664.68) {
-        irrf = calcPercent(baseCalcSalary, 22.5) - 646.13
-    } else {
-        irrf = calcPercent(baseCalcSalary, 27.5) - 869.36
-    }
-    return irrf
-}

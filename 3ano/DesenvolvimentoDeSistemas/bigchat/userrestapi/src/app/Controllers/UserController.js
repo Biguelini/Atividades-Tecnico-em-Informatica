c44a050ac3ddd2b64feb7c79fea8f7945c800231
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-05-14 19:05:25 -0300

    update and delete ok

--- 3ano/DesenvolvimentoDeSistemas/bigchat/userrestapi/src/app/Controllers/UserController.js
+++ 3ano/DesenvolvimentoDeSistemas/bigchat/userrestapi/src/app/Controllers/UserController.js
@@ -1,25 +1,34 @@
 const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
-
+const bcrypt = require('bcrypt')
 class UserController {
-    show(req, res) {
-        async function main() {
+    async show(req, res) {
+        try {
             const allUsers = await prisma.user.findMany()
             return res.status(200).json(allUsers)
-        }
-        main()
-            .catch((e) => {
-                throw e
-            })
-            .finally(async () => {
+        } catch (e) {
+            return console.log(e)
+        } finally {
+            ;async () => {
                 await prisma.$disconnect()
-            })
+            }
+        }
     }
-    create(req, res) {
-        async function main() {
-            const { email, name, password } = req.body
-            const createdUser = req.body
+    async create(req, res) {
+        try {
+            const { email, name } = req.body
+            let { password } = req.body
+            password = (await bcrypt.hash(password, 10)).toString()
+            const emailAlreadyUsed = await prisma.user.findUnique({
+                where: { email: email },
+            })
+            if (emailAlreadyUsed) {
+                return res
+                    .status(409)
+                    .json({ message: 'Email is already used' })
+            }
+            const createdUser = { name: name, email: email, password: password }
             await prisma.user.create({
                 data: {
                     name: name,
@@ -28,16 +37,76 @@ class UserController {
                 },
             })
             return res
-                .status(200)
+                .status(201)
                 .json({ message: 'User created', createdUser })
-        }
-        main()
-            .catch((e) => {
-                throw e
-            })
-            .finally(async () => {
+        } catch (e) {
+            return console.log(e)
+        } finally {
+            ;async () => {
                 await prisma.$disconnect()
-            })
+            }
+        }
+    }
+    async delete(req, res) {
+        try {
+            const id = parseInt(req.params.id)
+            try {
+                const deletedUser = await prisma.user.delete({
+                    where: { id: id },
+                })
+                return res.status(200).json({
+                    message: 'Usuário deletado com sucesso',
+                    deletedUser,
+                })
+            } catch (error) {
+                return res.status(404).json({
+                    error,
+                })
+            }
+        } catch (e) {
+            return console.log(e)
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    }
+    async update(req, res) {
+        try {
+            const id = parseInt(req.params.id)
+            const { email, name, password } = req.body
+            try {
+                const updatedUser = await prisma.user.update({
+                    where: { id: id },
+                    data: { email: email, name: name, password: password },
+                })
+                return res.status(200).json({
+                    message: 'Usuário atualizado com sucesso',
+                    updatedUser,
+                })
+            } catch (error) {
+                return res.status(404).json({
+                    error,
+                })
+            }
+        } catch (e) {
+            return console.log(e)
+        } finally {
+            ;async () => {
+                await prisma.$disconnect()
+            }
+        }
+    }
+    async login(req, res) {
+        const { email, password } = req.body
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+        })
+        const validated = await bcrypt.compare(password, user.password)
+        if (validated) {
+            return res.status(200).json({ message: 'conectado' })
+        }
+        return res.status(404).json({ message: 'usuário ou senha incorreto' })
     }
 }
 
--- 3ano/DesenvolvimentoDeSistemas/bigchat/userrestapi/src/routes.js
+++ 3ano/DesenvolvimentoDeSistemas/bigchat/userrestapi/src/routes.js
@@ -7,5 +7,8 @@ const routes = new Router()
 
 routes.get('/user', UserController.show)
 routes.post('/user', UserController.create)
+routes.post('/user/login', UserController.login)
+routes.delete('/user/:id', UserController.delete)
+routes.put('/user/:id', UserController.update)
 
 module.exports = routes
